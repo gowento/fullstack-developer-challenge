2fafca19d6991788b76d8dc0628d3afef3f4fb8b
Loris Guignard <loris.guignard@gmail.com>
2020-09-08 10:09:02 +0200

    Remove downloading/parsing of GB postal codes (not useful)

--- api/src/models/postal-codes.js
+++ api/src/models/postal-codes.js
@@ -1,6 +1,5 @@
 /* eslint-disable no-await-in-loop, no-restricted-syntax */
 
-import AdmZip from 'adm-zip';
 import csvParse from 'csv-parse';
 import EventEmitter from 'events';
 import fetch from 'node-fetch';
@@ -24,46 +23,6 @@ class PostalCodesDataContainer extends EventEmitter {
       logger.info('Postal codes data container: Initializing…');
 
       const data = [];
-
-      // 🇬🇧 Load and transform official postal code data as a CSV file from National Statistics Postcode Lookup
-      // https://geoportal.statistics.gov.uk/datasets/19fac93960554b5e90840505bd73917f
-
-      logger.info('Downloading Postal Codes Data for GB…');
-      const gbResponse = await fetch(
-        'https://www.arcgis.com/sharing/rest/content/items/19fac93960554b5e90840505bd73917f/data'
-      );
-
-      if (!gbResponse.ok) {
-        throw new Error(gbResponse.error);
-      }
-
-      const gbData = Buffer.from(await gbResponse.arrayBuffer());
-
-      // We are only interested in entries in the “Data/multi_csv” directory of the zip file
-      const gbZip = new AdmZip(gbData);
-      const gbZipEntries = gbZip.getEntries();
-
-      logger.info('Parsing Postal Codes Data for GB…');
-      for (const zipEntry of gbZipEntries) {
-        if (zipEntry.entryName.match(new RegExp('^Data/multi_csv/.*.csv'))) {
-          // Transform CSV entries to an array of objects with column names as keys
-          // Header of the CSV files for GB:
-          // pcd,pcd2,pcds,dointr,doterm,usertype,oseast1m,osnrth1m,osgrdind,oa11,cty,ced,laua,ward,hlthau,nhser,ctry,rgn,pcon,eer,teclec,ttwa,pct,nuts,park,lsoa11,msoa11,wz11,ccg,bua11,buasd11,ru11ind,oac11,lat,long,lep1,lep2,pfa,imd,calncv,stp
-          // Expected output:
-          // [{ pcd: "AB1 0AA", pcd2: "AB1  0AA", pcds: "AB1 0AA", … }, …]
-          const gbCsvEntries = await csvParseAsync(zipEntry.getData(), {
-            skip_lines_with_empty_values: true,
-            columns: true,
-          });
-
-          for (const entry of gbCsvEntries) {
-            data.push({
-              postalCode: entry.pcds,
-              countryCode: 'GB',
-            });
-          }
-        }
-      }
 
       // 🇫🇷️ Load and transform official postal code data as a CSV file from La Poste
       // https://datanova.legroupe.laposte.fr/explore/dataset/laposte_hexasmal/export/
